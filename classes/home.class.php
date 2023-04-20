9d29b183510a29c2543e9ed9255376e6fb3b87b3
juliasalvador19 <juliasalvador10@gmail.com>
2023-04-20 17:04:50 -0300

    .

--- classes/home.class.php
+++ classes/home.class.php
@@ -11,20 +11,15 @@
             $head = new Head();
             $html->addElemento($head);
             
-            $meta1 = new Meta('UTF-8', null, null, null);
-            $meta2 = new Meta(null, 'X-UA-Compatible', 'IE=edge', null);
-            $meta3 = new Meta(null, null, 'width=device-width, initial-scale=1.0', 'viewport');
-            $head->addElemento($meta1);
-            $head->addElemento($meta2);
-            $head->addElemento($meta3);
+            $head->addElemento(new Meta('UTF-8', null, null, null));
+            $head->addElemento(new Meta(null, 'X-UA-Compatible', 'IE=edge', null));
+            $head->addElemento(new Meta(null, null, 'width=device-width, initial-scale=1.0', 'viewport'));
 
             $title = new Title('Trabalho - Menu');
             $head->addElemento($title);
             
             $body = new Body();
             $html->addElemento($body);
-
-            echo $html;
 
             $link = new Link ('https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css', 'stylesheet');
             $head->addElemento($link);
